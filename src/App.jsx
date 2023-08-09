14b4289ff5fb14e1355d1344013effee5759272a
bilgramiraza <bilgramiraza@yahoo.com>
2023-08-09 11:17:33 +0530

    Setup a basic Fetch Function

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
+import { useEffect, useState } from "react";
 
 function App() {
+  const [name, setName] = useState('');
+
+  useEffect(()=>{
+    fetch('https://randomuser.me/api/',{mode:'cors'})
+      .then(res=>{
+        if(!res.ok) throw new Error('server Error');
+        return res.json();
+      })
+      .then(res=>setName(res.results[0].name.first))
+      .catch(err=>console.error(err));
+  },[]);
 
   return (
-    <>
-    </>
+    <main>
+      <h1>Hooks </h1>
+      <h1>{name}</h1>
+    </main>
   );
 }
 
